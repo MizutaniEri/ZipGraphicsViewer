7472a668c26b60b0d2a824d1c41cad73bb7cc64a
MizutaniEri <kirishima.mana.eva@gmail.com>
2017-08-08 17:35:09 +0900

    新規追加

---
+++ ZipGraphicViewer/ZipGraphicViewer/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.NextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BeforeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.FitScreenSizeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.WidthFitZoomToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(284, 262)
+        Me.Panel1.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem, Me.ToolStripSeparator2, Me.NextToolStripMenuItem, Me.BeforeToolStripMenuItem, Me.ToolStripSeparator3, Me.FitScreenSizeToolStripMenuItem, Me.WidthFitZoomToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(168, 176)
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(164, 6)
+        '
+        'NextToolStripMenuItem
+        '
+        Me.NextToolStripMenuItem.Name = "NextToolStripMenuItem"
+        Me.NextToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
+        Me.NextToolStripMenuItem.Text = "Next"
+        '
+        'BeforeToolStripMenuItem
+        '
+        Me.BeforeToolStripMenuItem.Name = "BeforeToolStripMenuItem"
+        Me.BeforeToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
+        Me.BeforeToolStripMenuItem.Text = "Before"
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(164, 6)
+        '
+        'FitScreenSizeToolStripMenuItem
+        '
+        Me.FitScreenSizeToolStripMenuItem.CheckOnClick = True
+        Me.FitScreenSizeToolStripMenuItem.Name = "FitScreenSizeToolStripMenuItem"
+        Me.FitScreenSizeToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
+        Me.FitScreenSizeToolStripMenuItem.Text = "Fit Screen Size"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(164, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "Bitmap|*.bmp|Jpeg|*.jpg|PNG|*.png"
+        '
+        'WidthFitZoomToolStripMenuItem
+        '
+        Me.WidthFitZoomToolStripMenuItem.CheckOnClick = True
+        Me.WidthFitZoomToolStripMenuItem.Name = "WidthFitZoomToolStripMenuItem"
+        Me.WidthFitZoomToolStripMenuItem.Size = New System.Drawing.Size(167, 22)
+        Me.WidthFitZoomToolStripMenuItem.Text = "Width Fit Zoom"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.Panel1)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "ZipGraphicViewer"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents OpenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents NextToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BeforeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripSeparator3 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents FitScreenSizeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents WidthFitZoomToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ ZipGraphicViewer/ZipGraphicViewer/Form1.vb
@@ -0,0 +1,227 @@
+﻿Imports System.IO.Compression
+Imports System.IO
+
+Public Class Form1
+    Private ActiveExt() As String = {".jpg", ".bmp", ".png", "gif"}
+    Private zipList As List(Of ZipArchiveEntry)
+    Private index As Integer = -1
+    Private zipFileName As String
+    Private imageFIleName As String
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Dim openDialog As New OpenFileDialog()
+        openDialog.Filter = "ZipFile|*.zip"
+        If openDialog.ShowDialog() = DialogResult.OK Then
+            ZipFileLoader(openDialog.FileName)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' ZIPファイルから画像ファイルの一覧を作成し、最初の画像を表示する
+    ''' </summary>
+    ''' <param name="FileName">ZIPファイル</param>
+    ''' <remarks></remarks>
+    Private Sub ZipFileLoader(FileName As String)
+        Using zipArc As ZipArchive = ZipFile.OpenRead(FileName)
+            zipFileName = FileName
+            zipList = zipArc.Entries.Where(Function(entity) ActiveExt.Contains(Path.GetExtension(entity.Name).ToLower())).ToList()
+            index += 1
+            ZipView(index)
+        End Using
+    End Sub
+
+    ''' <summary>
+    ''' ZIPファイルから指定のインデックスのファイルの画像を表示する
+    ''' </summary>
+    ''' <param name="index"></param>
+    ''' <remarks></remarks>
+    Private Sub ZipView(index As Integer)
+        imageFIleName = zipList(index).FullName
+        ' ZIP書庫を開く()
+        Using a As ZipArchive = ZipFile.OpenRead(zipFileName)
+            ' ZipArchiveEntryを取得する
+            Dim e As ZipArchiveEntry = a.GetEntry(imageFIleName)
+            Dim stream As Stream = e.Open()
+            PictureBox1.Image = Image.FromStream(stream, False, False)
+        End Using
+        If (FitScreenSizeToolStripMenuItem.Checked) Then
+            ScreenFitZoom()
+        ElseIf (WidthFitZoomToolStripMenuItem.Checked) Then
+            ScreenFitWidthZoom()
+        End If
+        Text = zipFileName & " (" & (index + 1) & "/" & zipList.Count & ") - " & zipList(index).Name
+    End Sub
+
+    Private Sub NextToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NextToolStripMenuItem.Click
+        NextListImage()
+    End Sub
+
+    Private Sub NextListImage()
+        If zipList.Count <= (index + 1) Then
+            index = 0
+        Else
+            index += 1
+        End If
+        ZipView(index)
+    End Sub
+
+    Private Sub BeforeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BeforeToolStripMenuItem.Click
+        BeforeListImage()
+    End Sub
+
+    Private Sub BeforeListImage()
+        If 0 > (index - 1) Then
+            index = zipList.Count - 1
+        Else
+            index -= 1
+        End If
+        ZipView(index)
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Close()
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim format As System.Drawing.Imaging.ImageFormat = System.Drawing.Imaging.ImageFormat.Jpeg
+            Select Case Path.GetExtension(SaveFileDialog1.FileName).ToLower()
+                Case ".jpg"
+                    format = System.Drawing.Imaging.ImageFormat.Jpeg
+                Case ".bmp"
+                    format = System.Drawing.Imaging.ImageFormat.Bmp
+                Case ".png"
+                    format = System.Drawing.Imaging.ImageFormat.Png
+            End Select
+
+            PictureBox1.Image.Save(SaveFileDialog1.FileName, format)
+        End If
+    End Sub
+
+    Private Function ZoomGraphic(newSize As Size)
+        '描画先とするImageオブジェクトを作成する
+        Dim canvas As New Bitmap(newSize.Width, newSize.Height)
+        'ImageオブジェクトのGraphicsオブジェクトを作成する
+        Dim g As Graphics = Graphics.FromImage(canvas)
+
+        '画像ファイルを読み込んで、Imageオブジェクトとして取得する
+        Dim img As Image = PictureBox1.Image
+        '画像のサイズを2倍にしてcanvasに描画する
+        g.DrawImage(img, 0, 0, newSize.Width, newSize.Height)
+        'Imageオブジェクトのリソースを解放する
+        img.Dispose()
+
+        'Graphicsオブジェクトのリソースを解放する
+        g.Dispose()
+        Return canvas
+    End Function
+
+    Private Sub GetScreenFitSize(ByRef imageSize As Size)
+        ' スクリーンサイズの取得
+        Dim Rect As Rectangle = Screen.GetWorkingArea(New Point(0, 0))
+        ' スクリーンサイズの幅と高さ
+        Dim screenX As Integer = Rect.Size.Width
+        Dim screenY As Integer = Rect.Size.Height
+        ' 画像の幅と高さ
+        Dim imageX As Integer = imageSize.Width
+        Dim imageY As Integer = imageSize.Height
+        Dim newX As Integer = 0
+        Dim newY As Integer = 0
+        Dim RX As Integer = 0
+        Dim RY As Integer = 0
+
+        ' 画像の比率に沿った幅と高さ計算
+        RX = imageX * screenY / imageY
+        RY = imageY * screenX / imageX
+
+        If ((RX < screenX) And (RY > screenY)) Then
+            newX = RX
+            newY = screenY
+        Else
+            newX = screenX
+            newY = RY
+        End If
+        imageSize.Width = newX
+        imageSize.Height = newY
+    End Sub
+
+    Private Sub GetScreenWidthFitSize(ByRef imageSize As Size)
+        ' スクリーンサイズの取得
+        Dim Rect As Rectangle = Screen.GetWorkingArea(New Point(0, 0))
+        ' スクリーンサイズの幅と高さ
+        Dim screenX As Integer = Rect.Size.Width
+        Dim screenY As Integer = Rect.Size.Height
+        ' 画像の幅と高さ
+        Dim imageX As Integer = imageSize.Width
+        Dim imageY As Integer = imageSize.Height
+        Dim newX As Integer = 0
+        Dim newY As Integer = 0
+        Dim RX As Integer = 0
+        Dim RY As Integer = 0
+
+        ' 画像の比率に沿った幅と高さ計算
+        RX = imageX * screenY / imageY
+        RY = imageY * screenX / imageX
+
+        'If ((RX < screenX) And (RY > screenY)) Then
+        '    newX = RX
+        '    newY = screenY
+        'Else
+        newX = screenX
+        newY = RY
+        'End If
+        imageSize.Width = newX
+        imageSize.Height = newY
+    End Sub
+
+    Private Sub FitScreenSizeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FitScreenSizeToolStripMenuItem.Click
+        If (FitScreenSizeToolStripMenuItem.Checked And WidthFitZoomToolStripMenuItem.Checked) Then
+            WidthFitZoomToolStripMenuItem.Checked = False
+        End If
+        If (FitScreenSizeToolStripMenuItem.Checked) Then
+            ScreenFitZoom()
+        Else
+            ZipView(index)
+        End If
+    End Sub
+
+    Private Sub WidthFitZoomToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles WidthFitZoomToolStripMenuItem.Click
+        If (FitScreenSizeToolStripMenuItem.Checked And WidthFitZoomToolStripMenuItem.Checked) Then
+            FitScreenSizeToolStripMenuItem.Checked = False
+        End If
+        If (WidthFitZoomToolStripMenuItem.Checked) Then
+            ScreenFitWidthZoom()
+        Else
+            ZipView(index)
+        End If
+    End Sub
+
+    Private Sub ScreenFitZoom()
+        Dim imageSize As Drawing.Size
+        imageSize.Width = PictureBox1.Image.Width
+        imageSize.Height = PictureBox1.Image.Height
+        GetScreenFitSize(imageSize)
+        PictureBox1.Image = ZoomGraphic(imageSize)
+    End Sub
+
+    Private Sub ScreenFitWidthZoom()
+        Dim imageSize As Drawing.Size
+        imageSize.Width = PictureBox1.Image.Width
+        imageSize.Height = PictureBox1.Image.Height
+        GetScreenWidthFitSize(imageSize)
+        PictureBox1.Image = ZoomGraphic(imageSize)
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        '左カーソルキーで次の画像表示
+        If e.KeyCode = Keys.Left Then
+            BeforeListImage()
+            '右カーソル or Backspaceキーで前の画像表示
+        ElseIf e.KeyCode = Keys.Right Or e.KeyCode = Keys.Back Then
+            NextListImage()
+        ElseIf e.KeyCode = Keys.Escape Then
+            Close()
+        End If
+    End Sub
+
+End Class
